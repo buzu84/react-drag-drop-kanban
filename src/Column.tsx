08825bf55c9d76224feec2ff3de61707c9788030
Magdalena Buzikiewicz <buzu@op.pl>
2021-05-05 16:15:48 +0200

    Added styles for add button & for the form elements.

--- src/Column.tsx
+++ src/Column.tsx
@@ -4,6 +4,7 @@ import { ColumnContainer, ColumnTitle } from "./styles"
 
 type ColumnProps = {
   text: string
+  children?: React.ReactNode;
 }
 
 export const Column: FC<ColumnProps> = ({ text, children }) => {
