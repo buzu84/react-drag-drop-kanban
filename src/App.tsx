a954bddf0d29b0cc7e9b4c0de5121f913c0fa76c
Magdalena Buzikiewicz <buzu@op.pl>
2021-05-05 16:44:58 +0200

    Rendering all the parts added so far.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React, { FC } from 'react';
 import { AppContainer } from "./styles"
-
+import { AddNewItem } from "./AddNewItem"
+import { Column } from "./Column"
+import { Card } from "./Card"
 
 export const App: FC = ({ children }) => {
   return (
     <AppContainer>
-    Columns will go here
+      <Column text="To Do">
+        <Card text="Redux" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Styled components" />
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
     </AppContainer>
     )
   }
--- src/Column.tsx
+++ src/Column.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react"
 import { ColumnContainer, ColumnTitle } from "./styles"
-// import { AddNewItem } from "./AddNewItem" 
+import { AddNewItem } from "./AddNewItem" 
 
 type ColumnProps = {
   text: string
@@ -12,6 +12,11 @@ export const Column: FC<ColumnProps> = ({ text, children }) => {
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
       {children}
+      <AddNewItem
+        toggleButtonText="+ Add another task"
+        onAdd={console.log}
+        dark
+      />
     </ColumnContainer>
   )
 }
