efe5591355d7d4b400c26b722c3d360b1386d3a8
Magdalena Buzikiewicz <buzu@op.pl>
2021-05-05 17:28:01 +0200

    Defined App State Context (React Context).

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import  App  from './App';
+import App from './App';
 // import {Counter} from './Counter';
+import { AppStateProvider } from "./state/AppStateContext"
 
 ReactDOM.render(
-  
-    <App />
-  ,
+
+  <React.StrictMode>
+    <AppStateProvider>
+      <App />
+    </AppStateProvider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
