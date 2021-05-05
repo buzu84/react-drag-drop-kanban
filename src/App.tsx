b5f99c3e2d55be4e8e2ad80bd8c5818a092c06ce
Magdalena Buzikiewicz <buzu@op.pl>
2021-05-05 17:08:28 +0200

    First reducer for counter(decrement/increment).

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
-import React, { FC } from 'react';
-import { AppContainer } from "./styles"
-import { AddNewItem } from "./AddNewItem"
-import { Column } from "./Column"
-import { Card } from "./Card"
+import React, { useReducer } from "react"
 
-export const App: FC = ({ children }) => {
-  return (
-    <AppContainer>
-      <Column text="To Do">
-        <Card text="Redux" />
-      </Column>
-      <Column text="In Progress">
-        <Card text="Learn Typescript" />
-      </Column>
-      <Column text="Done">
-        <Card text="Styled components" />
-      </Column>
-      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
-    </AppContainer>
-    )
+interface State {
+  count: number
+}
+
+type Action = { type: "increment" } | { type: "decrement" }
+
+const counterReducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 }
+    case "decrement":
+      return { count: state.count - 1 }
+    default:
+      throw new Error()
   }
+}
+
+const App = () => {
+  const [state, dispatch] = useReducer(counterReducer, { count: 0 })
+  return (
+    <>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatch({ type: "decrement" })}>
+        -
+  </button>
+      <button onClick={() => dispatch({ type: "increment" })}>
+        +
+  </button>
+    </>
+  )
+}
+
+export default App;
