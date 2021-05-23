76bc7b2873610ef4570672878eda290a0b323ec2
Magdalena Buzikiewicz <buzu@op.pl>
2021-05-23 13:57:27 +0200

    Updated Card and Column componenet with columnId props and types.

--- src/Card.tsx
+++ src/Card.tsx
@@ -3,8 +3,10 @@ import { CardContainer } from "./styles"
 type CardProps = {
   text: string
   id: string
-  }
-  
-export const Card = ({ text }: CardProps) => {
+  columnId: string
+  isPreview?: boolean
+}
+
+export const Card = ({ text, id, columnId, isPreview }: CardProps) => {
   return <CardContainer>{text}</CardContainer>
 }
