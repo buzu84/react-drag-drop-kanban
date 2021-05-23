ed63ea462359d1326467acc84b68a02ef45d6200
Magdalena Buzikiewicz <buzu@op.pl>
2021-05-23 22:05:52 +0200

    Load function (req to /load).

--- src/api.ts
+++ src/api.ts
@@ -18,3 +18,16 @@ export const save = (payload: AppState) => {
       }
     })
 }
+
+
+export const load = () => {
+  return fetch(`${process.env.REACT_APP_BACKEND_ENDPOINT}/load`).then(
+    (response) => {
+      if (response.ok) {
+        return response.json() as Promise<AppState>
+      } else {
+        throw new Error("Error while loading the state.")
+      }
+    }
+  )
+}
