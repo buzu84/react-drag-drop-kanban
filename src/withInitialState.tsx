d464169b45a97231a0ba3943878c7168e7bb7276
Magdalena Buzikiewicz <buzu@op.pl>
2021-05-24 17:34:21 +0200

    HOC updated with loading and error state and return WrappedComponent if the data has been loaded successfully.

--- src/withInitialState.tsx
+++ src/withInitialState.tsx
@@ -1,11 +1,14 @@
 import { useState, useEffect, ComponentType } from "react"
 import { AppState } from "./state/appStateReducer"
+import { load } from "./api"
 
 // define a type that will represent the props that have been injected.
 type InjectedProps = {
   initialState: AppState
 }
 
+// - generic type that accepts the TBaseProps type variable that will represent the original props type of the wrapped component
+// - Omit to remove the fields of the InjectedProps type from it
 type PropsWithoutInjected<TBaseProps> = Omit<
   TBaseProps,
   keyof InjectedProps
@@ -16,22 +19,37 @@ type PropsWithoutInjected<TBaseProps> = Omit<
 // Func returns a nameless function component
 // Omit - allows to create a new type that won’t have the keys of the InjectedProps type(the utility type Omit constructs a new type removing the keys that you provide to it:)
 
+// 3 diff states: pending, success, failure
 export function withInitialState<TProps>(
   WrappedComponent: ComponentType<
     PropsWithoutInjected<TProps> & InjectedProps
   >
 ) {
-  return (props: Omit<TProps, keyof InjectedProps>) => {
+  return (props: PropsWithoutInjected<TProps>) => {
     const [initialState, setInitialState] = useState<AppState>({
       lists: [],
       draggedItem: null
     })
-    // ...
-    return (
-      <WrappedComponent
-        {...props }
-        initialState={initialState}
-      />
-    )
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState<Error | undefined>()
+    useEffect(() => {
+      const fetchInitialState = async () => {
+        try {
+          const data = await load()
+          setInitialState(data)
+        } catch (e) {
+          setError(e)
+        }
+        setIsLoading(false)
+      }
+      fetchInitialState()
+    }, [])
+    if (isLoading) {
+      return <div>Loading</div>
+    }
+    if (error) {
+      return <div>{error.message}</div>
+    }
+    return <WrappedComponent {...props} initialState={initialState} />
   }
 }
